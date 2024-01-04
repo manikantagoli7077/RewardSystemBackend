64ae944553140c3700b8b0e3e3e4d32093840ef5
MANIKANTA Goli <goli.manikanta@soprasteria.com>
2024-01-04 18:15:14 +0530

    Pushing to deploy

--- src/main/java/com/spring/rewards/controller/RewardsController.java
+++ src/main/java/com/spring/rewards/controller/RewardsController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import com.spring.rewards.services.RewardsService;
 
 import jakarta.persistence.EntityNotFoundException;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class RewardsController {
